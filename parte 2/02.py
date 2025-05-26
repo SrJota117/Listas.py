ce2c2435d8f9932885fa7deebb1560eac5b32137
SrJota117 <104699972+SrJota117@users.noreply.github.com>
2025-05-26 02:07:38 -0300

    Add files via upload

---
+++ parte 2/02.py
@@ -0,0 +1,7 @@
+# Programa que mostra o maior de dois números
+a = float(input('Digite o primeiro número: '))
+b = float(input('Digite o segundo número: '))
+if a > b:
+    print('O maior número é:', a)
+else:
+    print('O maior número é:', b)
