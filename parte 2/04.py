b2915ab563f7a641a0ad6884be2929c70c9935df
SrJota117 <104699972+SrJota117@users.noreply.github.com>
2025-05-27 12:07:01 -0300

    Add files via upload

---
+++ parte 2/04.py
@@ -0,0 +1,8 @@
+# Programa que identifica o sexo
+letra = input('Digite F ou M: ').upper()
+if letra == 'F':
+    print('Feminino')
+elif letra == 'M':
+    print('Masculino')
+else:
+    print('Sexo Inválido')
