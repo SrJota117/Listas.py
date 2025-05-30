b34fbe0407a7d750037ffb43d6b6b3cebc5071ff
SrJota117 <104699972+SrJota117@users.noreply.github.com>
2025-05-29 22:32:40 -0300

    Add files via upload

---
+++ parte 2/18.py
@@ -0,0 +1,6 @@
+# Programa que verifica se o ano é bissexto
+def ano_bissexto(ano):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        return True
+    else:
+        return False
